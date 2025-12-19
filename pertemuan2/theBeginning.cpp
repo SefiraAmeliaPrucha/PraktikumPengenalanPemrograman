136badca4d83a82608657f4d9345d877e7ddc946
SefiraAmeliaPrucha <sefiraameliaprucha@gmail.com>
2025-12-20 02:41:06 +0700

    ada yang kelupaan

---
+++ pertemuan2/theBeginning.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama = "Sefira Amelia Prucha";
+    string NPM = "2517051060";
+
+    cout << "Selamat Datang di Praktikum Pengenalan Pemrograman" << endl;
+    cout << "Nama : " << nama << endl;
+    cout << "NPM  : " << NPM << endl;
+
+    cout << "Unila" << endl;
+    cout << "Be Nice ya" << endl;
+}
